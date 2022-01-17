fa4dd1680a94030040e874d57105072a19d67cd9
sunil-paswan <67237510+sunil-paswan@users.noreply.github.com>
2022-01-17 18:35:59 +0530

    Update pg_dump.sh

--- pg_dump.sh
+++ pg_dump.sh
@@ -3,8 +3,8 @@
 DATABASE="dvdrental_5"
 USERNAME="postgres"
 GCP_HOSTNAME="10.5.96.3"
-AWS_HOSTNAME="database-2.ccknmtsdcbj4.ap-south-1.rds.amazonaws.com"
-export PGPASSWORD="Bingo123"
+AWS_HOSTNAME=""
+export PGPASSWORD=""
 
 /home/13/bin/pg_dump -U $USERNAME -s -h $GCP_HOSTNAME -d $DATABASE > /tmp/db.dump
 psql -U $USERNAME -h $AWS_HOSTNAME -d $DATABASE < /tmp/db.dump > /tmp/pgdump.log
